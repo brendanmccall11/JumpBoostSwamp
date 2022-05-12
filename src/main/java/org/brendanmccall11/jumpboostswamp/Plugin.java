c488d2ab68c39643423a240bb5ab84e92c1674cf
Brendan McCall <brendanmccall3@gmail.com>
2022-05-11 21:09:34 -0600

    Added comments

--- src/main/java/org/brendanmccall11/jumpboostswamp/Plugin.java
+++ src/main/java/org/brendanmccall11/jumpboostswamp/Plugin.java
@@ -9,17 +9,18 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class Plugin implements Listener {
+
     public boolean isInSwamp (Player player) {
-        return player.getLocation().getWorld().getBiome(player.getLocation()).equals(Biome.SWAMP);
+        return player.getLocation().getWorld().getBiome(player.getLocation()).equals(Biome.SWAMP); // Tests if the player is in a swamp biome
     }
 
     @EventHandler
     public void main (PlayerMoveEvent event) {
 
         if (isInSwamp(event.getPlayer())) {
-            event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1000000, 2));
+            event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1000000, 2)); // Add jump boost when player enters the swamp biome
         } else if (!isInSwamp(event.getPlayer())) {
-            event.getPlayer().removePotionEffect(PotionEffectType.JUMP);
+            event.getPlayer().removePotionEffect(PotionEffectType.JUMP); // Removes jump boost when player enters the swamp biome
         }
     }
 }
